d9a1a5100a184d785391a730fe3ce938c66a379a
Spencer <sthompson.dev@outlook.com>
2023-12-01 08:44:18 -0700

    cleaned up stuff

--- src/dec1.c
+++ src/dec1.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int indexOf(char str[], char c) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == c){
-            return i;
-        }
-    }
-    return -1;
-}
-
 int main() {
     FILE *file;
     char filename[] = "dec1.txt";
@@ -28,32 +19,17 @@ int main() {
         for (int i = 0; buffer[i] != '\0'; i++) {
             int j = 0;
             int array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            // Check if digit and append as int to array
             if (buffer[i] >= '0' && buffer[i] <= '9') {
-                // printf("%c", buffer[i]);
-
                 array[j] = buffer[i] - '0';
                 j++;
-                // int first = 0;
-                // if (first == 0) {
-                    // printf("%c", buffer[i]);
-                    // first = buffer[i] - '0';
-                    // int last = first;
-                // } else {
-                    // int last = buffer[i] - '0';
-                // }
-            int length = 0;
-            for (int i = 0; array[i] != '\0'; i++) {
-                length++;
-            }
+                int length = 0;
+                for (int i = 0; array[i] != '\0'; i++) {
+                    length++;
+                }
             
             printf("%d", array[0]);
             printf("%d", array[length - 1]);
-            // for (int i = 0; array[i] != '\0'; i++) {
-            //     if (i == length - 1) {
-            //         printf("%d", array[i]);
-            //     }
-            // }
-            // printf("%d %d\n", first, last);
             }
         }
         printf("\n");
