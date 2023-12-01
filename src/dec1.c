0dd671b542b2124c62eab4bc293ffaf52b5b2d4b
Spencer <sthompson.dev@outlook.com>
2023-12-01 00:04:19 -0700

    idk man

--- src/dec1.c
+++ src/dec1.c
@@ -23,13 +23,40 @@ int main() {
         return 1;
     }
 
-    while (fgets(buffer, sizeof(buffer), file) != NULL){
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
         // printf("%s", buffer);
         for (int i = 0; buffer[i] != '\0'; i++) {
+            int j = 0;
+            int array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
             if (buffer[i] >= '0' && buffer[i] <= '9') {
-                printf("%s", buffer[i]);
+                // printf("%c", buffer[i]);
+
+                array[j] = buffer[i] - '0';
+                j++;
+                // int first = 0;
+                // if (first == 0) {
+                    // printf("%c", buffer[i]);
+                    // first = buffer[i] - '0';
+                    // int last = first;
+                // } else {
+                    // int last = buffer[i] - '0';
+                // }
+            int length = 0;
+            for (int i = 0; array[i] != '\0'; i++) {
+                length++;
+            }
+            
+            printf("%d", array[0]);
+            printf("%d", array[length - 1]);
+            // for (int i = 0; array[i] != '\0'; i++) {
+            //     if (i == length - 1) {
+            //         printf("%d", array[i]);
+            //     }
+            // }
+            // printf("%d %d\n", first, last);
             }
         }
+        printf("\n");
     }
     
     fclose(file);
