e957d8d665f8387676761ebdb973665d3f71f5de
Spencer Thompson <sthompson.dev@outlook.com>
2023-12-01 12:41:46 -0700

    part one success!

--- src/dec1.c
+++ src/dec1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int length(int array[]) {
+    int length = 0;
+    for (int i = 0; array[i] != 0; i++) {
+        length++;
+    }
+    return length;
+}
+
 int main() {
     FILE *file;
     char filename[] = "dec1.txt";
@@ -14,27 +22,27 @@ int main() {
         return 1;
     }
 
+    int total = 0;
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
         // printf("%s", buffer);
+        int j = 0;
+        int array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         for (int i = 0; buffer[i] != '\0'; i++) {
-            int j = 0;
-            int array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
             // Check if digit and append as int to array
             if (buffer[i] >= '0' && buffer[i] <= '9') {
                 array[j] = buffer[i] - '0';
                 j++;
-                int length = 0;
-                for (int i = 0; array[i] != '\0'; i++) {
-                    length++;
-                }
-            
-            printf("%d", array[0]);
-            printf("%d", array[length - 1]);
+
             }
         }
+        int length_a = length(array);
+        total += (array[0] * 10) + array[length_a-1];
+        printf("%d", array[0]);
+        printf("%d", array[length_a-1]);
         printf("\n");
     }
-    
+    // answer yay
+    printf("%d", total);
     fclose(file);
 
     return 0;
