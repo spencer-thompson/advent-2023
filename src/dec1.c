55d969c9e808ec8c7bf9174d189a3d5141431072
Spencer <sthompson.dev@outlook.com>
2023-12-01 20:25:11 -0700

    I did it, solution is horrible lol

--- src/dec1.c
+++ src/dec1.c
@@ -11,10 +11,15 @@ int length(int array[]) {
     return length;
 }
 
-int check_word(char arr[]) {
-    if (strstr(arr, "one") != NULL) {
-        return 0;
-    }
+
+typedef struct {
+    char word[10];
+    int val;
+} numWord;
+
+
+int check_word(char arr[], numWord allWords[]) {
+
 }
 
 int main() {
@@ -24,28 +29,112 @@ int main() {
     
     file = fopen(filename, "r");
     
-
+    // File stuff
     if (file == NULL) {
         printf("Error opening file\n");
         return 1;
+    }
+
+    numWord words[9];
+    char *numbers[] = {"one!", "two!", "three!", "four!", "five!", "six!", "seven!", "eight!", "nine!"};
+    int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    for (int i = 0; i < 9; i++) {
+        strcpy(words[i].word, numbers[i]);
+        words[i].val = nums[i];
     }
 
     int total = 0;
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
         // printf("%s", buffer);
         int j = 0;
+        int k = 0;
         int array[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         
 
         //check if word is digit
         //TODO
+        
 
-        for (int i = 0; buffer[i] != '\0'; i++) {
+        for (int i = 0; buffer[i + k] != '\0'; i++) {
             // Check if digit and append as int to array
+            k = 0;
             if (buffer[i] >= '0' && buffer[i] <= '9') {
                 array[j] = buffer[i] - '0';
                 j++;
 
+            } else {
+                switch (buffer[i + k]) {
+                    case 'o':
+                        if (buffer[i + k + 1] == 'n' && buffer[i + k + 2] == 'e') {
+                            array[j] = 1;
+                            j++;
+                        }
+                        break;
+                    case 't':
+                        if (buffer[i + k + 1] == 'w' && buffer[i + k + 2] == 'o') {
+                            array[j] = 2;
+                            j++;
+                        } else if (buffer[i + k + 1] == 'h' && buffer[i + k + 2] == 'r' && buffer[i + k + 3] == 'e' && buffer[i + k + 4] == 'e') {
+                            array[j] = 3;
+                            j++;
+                        }
+                        break;
+                    case 'f':
+                        if (buffer[i + k + 1] == 'o' && buffer[i + k + 2] == 'u' && buffer[i + k + 3] == 'r') {
+                            array[j] = 4;
+                            j++;
+                        } else if (buffer[i + k + 1] == 'i' && buffer[i + k + 2] == 'v' && buffer[i + k + 3] == 'e') {
+                            array[j] = 5;
+                            j++;
+                        }
+                        break;
+                    case 's':
+                        if (buffer[i + k + 1] == 'i' && buffer[i + k + 2] == 'x') {
+                            array[j] = 6;
+                            j++;
+                        } else if (buffer[i + k + 1] == 'e' && buffer[i + k + 2] == 'v' && buffer[i + k + 3] == 'e' && buffer[i + k + 4] == 'n') {
+                            array[j] = 7;
+                            j++;
+                        }
+                        break;
+                    case 'e':
+                        if (buffer[i + k + 1] == 'i' && buffer[i + k + 2] == 'g' && buffer[i + k + 3] == 'h' && buffer[i + k + 4] == 't') {
+                            array[j] = 8;
+                            j++;
+                        }
+                        break;
+                    case 'n':
+                        if (buffer[i + k + 1] == 'i' && buffer[i + k + 2] == 'n' && buffer[i + k + 3] == 'e') {
+                            array[j] = 9;
+                            j++;
+                        }
+                        break;
+                    default:
+                        k = 0;
+                }
+                // int flag = 0;
+
+                // for (int m = 0; m < 9 && !flag; m++) {
+                //     k = 0;
+                //     while (buffer[i + k] != '\0') {
+                //         if (buffer[i + k] == '\0' && words[m].word[k] == '\0') {
+                //             array[j] = words[m].val;
+                //             flag = 1;
+                //             break;
+                //         } else if (buffer[i + k] == words[m].word[k]) {
+                //             k++;
+                //         } else if (words[m].word[k] == '!') {
+                //             array[j] = words[m].val;
+                //             flag = 1;
+                //             break;
+                //         } else {
+                //             k = 0;
+                //             break;
+                //         }
+                //     }
+                // }
+                // check if next set of chars is a "digit" then append that digit
             }
         }
         int length_a = length(array);
